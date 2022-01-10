84fb3bd20298baff5cf789f48632b030e01ee2f2
msre0001 <zaidreshamwale2015@gmail.com>
2022-01-10 09:53:01 +0530

    MainActivity.kt changed

--- app/src/main/java/com/synchronoss/water_reminder/MainActivity.kt
+++ app/src/main/java/com/synchronoss/water_reminder/MainActivity.kt
@@ -1,11 +1,67 @@
 package com.synchronoss.water_reminder
 
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import com.daimajia.androidanimations.library.Techniques
+import com.daimajia.androidanimations.library.YoYo
+import com.google.android.material.snackbar.Snackbar
+import com.synchronoss.water_reminder.utils.AppUtils
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var totalIntake: Int = 0
+    private var inTook: Int = 0
+    private lateinit var sharedPref: SharedPreferences
+    private lateinit var dateNow: String
+    private var snackbar: Snackbar? = null
+    private var doubleBackToExitPressedOnce = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        totalIntake = sharedPref.getInt(AppUtils.TOTAL_INTAKE, 0)
+//        date
+        dateNow = AppUtils.getCurrentDate()!!
     }
+
+
+    private fun setWaterLevel(inTook: Int, totalIntake: Int) {
+
+        YoYo.with(Techniques.SlideInDown)
+            .duration(500)
+            .playOn(tvIntook)
+        tvIntook.text = "$inTook"
+        tvTotalIntake.text = "/$totalIntake ml"
+        val progress = ((inTook / totalIntake.toFloat()) * 100).toInt()
+        YoYo.with(Techniques.Pulse)
+            .duration(500)
+            .playOn(intakeProgress)
+        intakeProgress.currentProgress = progress
+//        if ((inTook * 100 / totalIntake) > 140) {
+//            Snackbar.make(main_activity_parent, "You achieved the goal", Snackbar.LENGTH_SHORT)
+//                .show()
+//        }
+    }
+
+    override fun onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed()
+            return
+        }
+
+        this.doubleBackToExitPressedOnce = true
+        Snackbar.make(
+            this.window.decorView.findViewById(android.R.id.content),
+            "Please click BACK again to exit",
+            Snackbar.LENGTH_SHORT
+        ).show()
+
+        Handler().postDelayed({ doubleBackToExitPressedOnce = false }, 1000)
+    }
+
+
 }
